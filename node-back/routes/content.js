c4f4ebc2dc56a245b72392c9a5c02f3e1b7a798d
Kubat Aytekin <kubat.aytekin@student.ecp.fr>
2018-11-16 03:07:59 +0100

    deleted taches.js

--- node-back/routes/content.js
+++ node-back/routes/content.js
@@ -5,7 +5,7 @@ const taches = require('./taches.json');
 
 router.get('/', (req,res,next) => {console.log(req.session.id,req.session);res.send(req.session.auth);next()});
 
-router.get('/taches', (req,res) => {console.log("ca passe session");res.json(taches);});
+
 
 
 
